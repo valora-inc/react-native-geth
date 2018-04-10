188f20c7d67e90dd16cb1d44c3ef5d5bae835073
Yass <y.kassy@me.com>
2018-04-10 22:29:28 +0100

    Update Geth v1.8.3
    Update to latest stable release of Geth - Supreme Gardening Equipment (v1.8.3)

--- scripts/rnpm-postlink.js
+++ scripts/rnpm-postlink.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const appBuildGradlePath = path.join('android', 'app', 'build.gradle')
-const defaultCompileStatement = "compile 'org.ethereum:geth:1.8.2'"
-const requiredCompileStatement = "compile 'org.ethereum:geth:1.8.2'"
+const defaultCompileStatement = "compile 'org.ethereum:geth:1.8.3'"
+const requiredCompileStatement = "compile 'org.ethereum:geth:1.8.3'"
 let buildGradleContents = fs.readFileSync(appBuildGradlePath, 'utf8')
 buildGradleContents = buildGradleContents.replace(defaultCompileStatement, requiredCompileStatement)
 fs.writeFileSync(appBuildGradlePath, buildGradleContents)
